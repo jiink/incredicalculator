0d17a27f7a65b562783d6f438a1ad74bedb24529
Jacob C <jacob.j.carlson03@gmail.com>
2025-10-22 21:42:38 -0700

    get the rp2350 project building with a reference to incredicalculator core

--- incredicalculator_rp/src/main.rs
+++ incredicalculator_rp/src/main.rs
@@ -5,12 +5,19 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
 use embassy_time::Timer;
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
+use incredicalculator_core::{IcKey, IcPlatform, IcState};
+use embedded_alloc::LlffHeap as Heap;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
 
 // Program metadata for `picotool info`.
 // This isn't needed, but it's recomended to have these minimal entries.
@@ -27,10 +34,19 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    }
+
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_25, Level::Low);
-
+    
+    let mut icalc: IcState = IcState::new();
     loop {
+
         info!("led on!");
         led.set_high();
         Timer::after_millis(250).await;
