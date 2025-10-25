c9cbaf7864cb588f950f32cbf675d9d75531092c
Jacob C <jacob.j.carlson03@gmail.com>
2025-10-25 16:17:29 -0700

    failing rp display attempt

--- incredicalculator_rp/src/main.rs
+++ incredicalculator_rp/src/main.rs
@@ -1,20 +1,24 @@
-//! This example test the RP Pico on board LED.
-//!
-//! It does not work with the RP Pico W board. See `blinky_wifi.rs`.
-
 #![no_std]
 #![no_main]
 
 extern crate alloc;
 
+use core::cell::RefCell;
+
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::{gpio::{self, Pull}, spi};
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_time::Timer;
-use gpio::{Level, Output};
+use gpio::{Level, Output, Input};
 use {defmt_rtt as _, panic_probe as _};
 use incredicalculator_core::{IcKey, IcPlatform, IcState};
 use embedded_alloc::LlffHeap as Heap;
+use embassy_sync::blocking_mutex::Mutex;
+use display_interface_spi::SPIInterface;
+use mipidsi::{models::ST7789, options::{ColorInversion, Orientation}, Builder};
+use embassy_time::Delay;
+use embedded_graphics::pixelcolor::Rgb565;
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
@@ -24,9 +28,9 @@ static HEAP: Heap = Heap::empty();
 #[unsafe(link_section = ".bi_entries")]
 #[used]
 pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
-    embassy_rp::binary_info::rp_program_name!(c"Blinky Example"),
+    embassy_rp::binary_info::rp_program_name!(c"Incredicalculator"),
     embassy_rp::binary_info::rp_program_description!(
-        c"This example tests the RP Pico on board LED, connected to gpio 25"
+        c"It's not done, sir."
     ),
     embassy_rp::binary_info::rp_cargo_version!(),
     embassy_rp::binary_info::rp_program_build_attribute!(),
@@ -42,11 +46,32 @@ async fn main(_spawner: Spawner) {
     }
 
     let p = embassy_rp::init(Default::default());
+    let mut btn = Input::new(p.PIN_26, Pull::Up);
     let mut led = Output::new(p.PIN_25, Level::Low);
-    
+    let mosi = p.PIN_7;
+    let clk = p.PIN_6;
+    let chip_sel = p.PIN_5;
+    let data_cmd = p.PIN_8;
+    let disp_reset = p.PIN_4;
+    let mut spi_cfg = spi::Config::default();
+    spi_cfg.frequency = 2_000_000;
+    spi_cfg.phase = spi::Phase::CaptureOnSecondTransition;
+    spi_cfg.polarity = spi::Polarity::IdleHigh;
+    let spi = embassy_rp::spi::Spi::new_blocking_txonly(p.SPI0, clk, mosi, spi_cfg);
+    let spi_bus = Mutex::new(RefCell::new(spi));
+    let disp_spi = SpiDeviceWithConfig::new(&spi_bus, Output::new(chip_sel, Level::High), spi_cfg);
+    let mut buffer = [0u8; 640]; // todo: play with the size of this thing
+    let disp_iface = SPIInterface::new(spi, data_cmd);
+    let mut display = Builder::new(ST7789, disp_iface)
+        .display_size(240, 320)
+        .reset_pin(disp_reset)
+        .orientation(Orientation::new())
+        .init(&mut Delay)
+        .unwrap();
+    display.clear(Rgb565::RED).unwrap();
     let mut icalc: IcState = IcState::new();
     loop {
-
+        btn.wait_for_falling_edge().await;
         info!("led on!");
         led.set_high();
         Timer::after_millis(250).await;
